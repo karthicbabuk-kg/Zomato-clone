577f5a07ba3c69ba2fbe3a3e10efd6a8bddb8db9
karthicbabuk-kg <karthicbabu333@gmail.com>
2024-05-04 12:34:57 +0530

    First Commit

---
+++ zomato-clone/src/components/common/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../footer/footer.css";
+
+const Footer = () => {
+  return <div className="absolute-center">Made with ❤️ by KarthicBabu</div>;
+};
+
+export default Footer;
